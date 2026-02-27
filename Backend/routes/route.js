a11d0aaa904def7ed8e1cfd7a94cefecc849213a
Anmol Kumar <anmolkrjee1@gmail.com>
2026-02-27 20:48:23 +0530

    Add TaskUpdate controller and route for updating tasks

--- Backend/routes/route.js
+++ Backend/routes/route.js
@@ -4,12 +4,15 @@ import Register from "../controllers/Register.js"
 import Login from "../controllers/Login.js"
 import TaskAdd from "../controllers/TaskAdd.js"
 import TaskWithId from "../controllers/TaskWithId.js"
+import TaskUpdate from "../controllers/TaskUpdate.js"
+
 
 
 router.post('/api/auth/register', Register)
 router.post('/api/auth/login', Login)
 router.post('/api/addtasks', TaskAdd)
 router.get('/api/tasks/:id', TaskWithId)
+router.put('/api/tasks/:id', TaskUpdate)
 
 
 export default router
