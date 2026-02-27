31dcaaddf0e34dedbd7e7d2da429dff27c696fbb
Anmol Kumar <anmolkrjee1@gmail.com>
2026-02-27 21:30:35 +0530

    Add AddSubTask controller and route for adding subtasks to a task

--- Backend/routes/route.js
+++ Backend/routes/route.js
@@ -7,6 +7,7 @@ import TaskWithId from "../controllers/TaskWithId.js"
 import TaskUpdate from "../controllers/TaskUpdate.js"
 import TaskDelete from "../controllers/TaskDelete.js"
 import GetAllTask from "../controllers/GetAllTask.js"
+import AddSubTask from "../controllers/AddSubTasks.js"
 
 
 
@@ -16,6 +17,7 @@ router.post('/api/addtasks', TaskAdd)
 router.get('/api/tasks/:id', TaskWithId)
 router.put('/api/tasks/:id', TaskUpdate)
 router.delete('/api/tasks/:id', TaskDelete)
-
+router.post('/api/tasks/:id/subtasks', AddSubTask)
+router.post('/api/gettasks', GetAllTask)
 
 export default router
