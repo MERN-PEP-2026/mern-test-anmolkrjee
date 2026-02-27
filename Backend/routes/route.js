2f9f84b557b89a4cb3a085525967e3117ba574af
Anmol Kumar <anmolkrjee1@gmail.com>
2026-02-27 20:32:38 +0530

    Add TaskWithId controller and route for fetching task by ID

--- Backend/routes/route.js
+++ Backend/routes/route.js
@@ -3,11 +3,13 @@ const router = express.Router()
 import Register from "../controllers/Register.js"
 import Login from "../controllers/Login.js"
 import TaskAdd from "../controllers/TaskAdd.js"
+import TaskWithId from "../controllers/TaskWithId.js"
 
 
 router.post('/api/auth/register', Register)
 router.post('/api/auth/login', Login)
 router.post('/api/addtasks', TaskAdd)
+router.get('/api/tasks/:id', TaskWithId)
 
 
 export default router
